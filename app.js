cc7217b063d9e441b617703eb29c47e9b4812579
Shira Rose Espo <shiraroseespo@Shiras-MBP.lan>
2016-07-14 00:36:00 -0400

    added gradient bg, 5 panels, fish and jquery event

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
 
+$(document).ready(function(){     // when the document is ready, I want this function to happen
+  $("#panel3").on("mouseenter", function(){
+    $("#fish").fadeIn()
+  });
 
 
-/* JS COMMENTS N MULTIPLE LINES*/
-
-/* JS telling jQuery when its time to run a script 
-(so it doesn’t start immediately and take a while to run through the file)*/
-
-<script>
+});
 
 
-$(document).ready(function(){
-	$("p").mouseenter(function(){
-		$("p").css("background-color", "yellow");
-
-	});
-
-</script>
-
-
-
-
-$("#background").mouseleave(function({
-    $(this).css("border", "none");});
-
-
- $("#background").mouseenter(function(){
-    $(this).css("border", "solid 2px #990033");});
-/* above says: when the mouse enters the background space, show a pink border*/
- $("#background").mouseleave(function(){
-    $(this).css("border", "none");});
-
-$(document ).ready(function(){
-    $("#content").fadeOut();
-
-$( "p" ).text( "The DOM is now loaded and can be manipulated." );
-  }

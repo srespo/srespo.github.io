313e88224e491111081b63f81b15e6c2b495ae75
Shira Rose Espo <shiraroseespo@Shiras-MBP.lan>
2016-07-24 21:12:10 -0400

    trying to get auto-scroll to work

--- chat/chat_client.js
+++ chat/chat_client.js
@@ -1,53 +1,57 @@
 //client side javascript file
-$(document).ready(function(){
-var socket = io("http://localhost:3000"); //any time we reference this variable (socket) we are referencing this connection between zack and the pizza place
-	$("#chat-start").click(function(){
-		$.ajax({
-			url:"http://localhost:3000/get_archive",
-			success: function(data){
-				$("#chat-log ul li").remove();
-				for(var i=0;i<data.length;i++)
+$(document).ready(function() {
+    var socket = io("http://localhost:3000"); //any time we reference this variable (socket) we are referencing this connection between zack and the pizza place
+    $("#chat-start").click(function() {
+        $.ajax({
+            url: "http://localhost:3000/get_archive",
+            success: function(data) {
+                $("#chat-log ul li").remove();
+                for (var i = 0; i < data.length; i++) {
+                    var msg = data[i];
+                    if (msg.username == $("#chat-name").val()) {
+                        $("#chat-log ul").append('<li class="me-line"><b>' + msg.username + "</b>: " + msg.text + "</li>");
+                    } else {
+                        $("#chat-log ul").append('<li class="them-line"><b>' + msg.username + "</b>: " + msg.text + "</li>");
+                    }
+                }
 
-		{
-			var msg = data[i];
-			if(msg.username == $("#chat-name").val()) {
-				$("#chat-log ul").append("<li class='me-line'><b>" + msg.username + "</b>: " + msg.text + "</li>");
-			} else {
-				$("#chat-log ul").append("<li class='them-line'><b>" + msg.username + "</b>: " + msg.text + "</li>");
-			}
+            }
 
-		}
-	}
+        });
+    });
+    $("#chat-form").submit(function() {
+        var messageObject = {};
+        messageObject.username = $("#chat-name").val();
+        messageObject.text = $("#chat-input").val();
 
-});
+        socket.emit("chat message", messageObject);
+        $("#chat-input").val(""); //set the value of the text box to "nothing" after message is submitted
+        return false; //dont reload the page
+    });
 
-});
+    socket.on("chat message", function(msg) {
 
-$("#chat-form").submit(function(){
-	var messageObject = {};
-		messageObject.username = $("#chat-name").val();
-		messageObject.text = $("#chat-input").val();
+        if (msg.username == $("#chat-name").val()) {
+            $("#chat-content").append('<li class="me-line"><b>' + msg.username + '</b>: ' + msg.text + '</li>').ready(function() {
+            	
+            });
+        } else {
+            $("#chat-content").append("<li class='them-line'><b>" + msg.username + "</b>: " + msg.text + "</li>");
+        }
 
-	socket.emit("chat message", messageObject);
-	$("#chat-input").val(""); //set the value of the text box to "nothing" after message is submitted
-	return false; //dont reload the page
-});
+        $("#chat-content img").last().ready().each(function() {
+        	console.log(this);
+        	$(this).load(function() {
+        		getChat();
+        	});
+        });
 
-socket.on("chat message", function(msg){
-	$("#ul").height();
-	if(msg.username == $("#chat-name").val()) {
-		$("#chat-log ul").append("<li class='me-line'><b>" + msg.username + "</b>: " + msg.text + "</li>");
-	} else {
-		$("#chat-log ul").append("<li class='them-line'><b>" + msg.username + "</b>: " + msg.text + "</li>");
-	}
+        function getChat() {
+            console.log("getChat function called");
+            var div = $("#chat-content");
+            console.log(div.prop('scrollHeight'), div.height());
+            div.scrollTop(div.prop('scrollHeight'));
+        }
 
-});
+    });
 })
-
-
-
-
-
-
-
-//this above code WORKS!as client side server file
--- chat/chat_server.js
+++ chat/chat_server.js
@@ -2,82 +2,49 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var request = require('request'); //added
-
+var request = require('request');
 var chatArchive = [];
+
 
 //following is copied from etherpad
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "OPTIONS,HEAD,GET,PUT");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "OPTIONS,HEAD,GET,PUT");
+    next();
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World im here!');
+app.get('/', function(req, res) {
+    res.send('Hello World im here!');
 });
 
-// Youtube api key: AIzaSyDVXX5EIlVai2VfBn8Hia9QnkE7rlezEPI
-// youtube video link: https://www.youtube.com/watch?v={video_id}
-// Youtube API: https://www.googleapis.com/youtube/v3/search?part=snippet&q=cat&key={YOUR_API_KEY}
 var user, requestOptions;
-io.on('connection', function(socket){ //when socket makes a connection from the client to the server
-	
-	socket.on("chat message", function(msg){
+io.on('connection', function(socket) { //when socket makes a connection from the client to the server
 
-		var msgWords = msg.text.split(" ");
-		var lastWord = msgWords[msgWords.length - 1];
-		var randomMsgWord = msgWords[Math.floor(Math.random() * msgWords.length)];
+        socket.on("chat message", function(msg) {
 
-		if (user != msg.username) { // new speaker
-			requestOptions = {
-				url: 'http://api.giphy.com/v1/gifs/random',
-				qs: {
-					api_key: 'dc6zaTOxFJmzC',
-					tag: randomMsgWord
-				}
-			};
-			request(requestOptions, function(err, res, body) {
-				if (err) { throw Error(err); }
-				body = JSON.parse(body);
-				console.log('new speaker');
-				console.log('body', body.data.image_url);
+            var msgWords = msg.text.split(" ");
+            //var lastWord = msgWords[msgWords.length - 1];//
+            var randomMsgWord = msgWords[Math.floor(Math.random() * msgWords.length)];
 
-				msg.text = msg.text + '<img src="' + body.data.image_url + '"/>';
-				chatArchive.push(msg);
-				io.emit("chat message", msg);
-				
-			});
+            requestOptions = {
+                url: 'http://api.giphy.com/v1/gifs/random',
+                qs: {
+                    api_key: 'dc6zaTOxFJmzC',
+                    tag: randomMsgWord
+                }
+            };
+            request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + randomMsgWord, function(err, res, body) {
+                body = JSON.parse(body);
+                msg.text = msg.text + '<br>' + '<br>' + '<img src="' + body.data.image_url + '" width="400" /><br>';
+                chatArchive.push(msg);
+                io.emit("chat message", msg);
 
-		} else {
-
-			requestOptions = { url: "https://www.googleapis.com/youtube/v3/search",
-				qs: { 
-					part: "snippet",
-					q: lastWord,
-					key: 'AIzaSyDVXX5EIlVai2VfBn8Hia9QnkE7rlezEPI'
-				}
-			};
-			request(requestOptions, function(err, res, body) {
-				if (err) { throw Error(err); }
-				body = JSON.parse(body);
-
-				var videoId = body.items[0].id.videoId;
-				var title = body.items[0].snippet.title;
-				msg.text = msg.text + ' [Youtube: <a href="https://www.youtube.com/watch?v=' + videoId + '">' + title + '</a>]';
-				chatArchive.push(msg);
-				io.emit("chat message", msg);
-				
-			});
-		}
-		user = msg.username;
-
-	});
-});
-
-//dc6zaTOxFJmzC giphy 
-// random gif - http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=
+            });
+        });
+    })
+    //dc6zaTOxFJmzC giphy 
+    // random gif - http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=
 
 
 /*request("https://api.spotify.com/v1/search?q=track:" + lastWord + "&type=track", function(err, resp, body) {
@@ -92,16 +59,15 @@ io.on('connection', function(socket){ //when socket makes a connection from the 
 	});*/
 
 app.get('/get_archive', function(req, res) {
-	res.send(chatArchive); //show the previous chat lines from past converstions with other 
+    res.send(chatArchive); //show the previous chat lines from past converstions with other 
 });
 
 app.use(express.static('public'));
 
 app.get('chat', function(req, res) {
-	res.sendfile(_dirname + 'chat_client.html');
+    res.sendfile(_dirname + 'chat_client.html');
 });
 
 http.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
-
